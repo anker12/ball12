445872de040609c9135e12a78e0ec0947258b8c4
Tobias <Tobias.as@hotmail.com>
2020-10-07 14:24:53 +0200

    14:25 - 07-10-2020

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
 
@@ -57,9 +55,61 @@ public class Main {
         System.out.println("Der er "+tbList.size()+" tennisbolde, ud af "+balls.size()+" bolde ialt");
     }
 
-    public static void main(String[] args){
+    public static void fjernTennisbolde(){
         List<Ball> balls = getBalls();
+        System.out.println("____________________________________");
+        System.out.println(balls.size());
+        for (int i=balls.size()-1; i>=0; i--){
+            Ball b = balls.get(i);
+            if (b.getClass().getName()=="TennisBall"){
+                balls.remove(b);
 
+            }
+        }
+        System.out.println(balls.size());
+        System.out.println(balls);
+    }
+
+    public static void hashmap(){
+        ArrayList<Ball> balls = getBalls();
+
+        HashMap<String, ArrayList<Ball>> map = new HashMap<>();
+
+        ArrayList<Ball> fodbolde = new ArrayList<>();
+        fodbolde.addAll(balls);
+        for (int i=fodbolde.size()-1; i>=0; i--){
+            Ball b = fodbolde.get(i);
+            if (b.getClass().getName()=="TennisBall"){
+                fodbolde.remove(b);
+
+            }
+        }
+        ArrayList<Ball> tennisbolde = new ArrayList<>();
+        tennisbolde.addAll(balls);
+        for (int i=tennisbolde.size()-1; i>=0; i--){
+            Ball b = tennisbolde.get(i);
+            if (b.getClass().getName()=="FootBall"){
+                tennisbolde.remove(b);
+
+            }
+        }
+        map.put("fodbolde",fodbolde);
+        map.put("tennisbolde",tennisbolde);
+
+        //System.out.println(map.get("fodbolde"));
+        //System.out.println(map.get("tennisbolde"));
+
+
+        for (String str1: map.keySet()){
+            ArrayList<Ball> val = map.get(str1);
+            System.out.println(val);
+        }
+
+    }
+
+    public static void main(String[] args){
+        ArrayList<Ball> balls = getBalls();
+        hashmap();
 
     }
 }
